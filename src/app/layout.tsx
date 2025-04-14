39382582b65a57e040fff4975f6bb6653a4ee4bd
prad.alpukat <pradityaaldi.s@gmail.com>
2025-04-14 23:46:23 +0700

    Fix import path for Navbar component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '../styles/custom.css';
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/block/Navbar";
 import Footer from "@/components/block/Footer";
 
 const geistSans = Geist({
